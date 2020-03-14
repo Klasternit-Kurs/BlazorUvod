a91d69028fd25451286d169478635a3eb9af27f7
Dragan Pepic <d.pepic@hotmail.com>
2020-03-14 20:14:22 +0100

    Unos

---
+++ Client/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Client
+{
+	public class TestKlasa
+	{
+		public int Br { get; set; }
+		public string Txt { get; set; }
+		public bool Brisanje { get; set; }
+
+		public TestKlasa(int x, string t)
+		{
+			Br = x;
+			Txt = t;
+		}
+	}
+}
